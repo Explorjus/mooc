2b5228e09a6b17caec8877267cdace9a357a3d65
Explorjus <ja.kubson022@gmail.com>
2025-01-03 17:05:21 +0100

    Add files via upload

---
+++ part04-02_line/src/line.py
@@ -0,0 +1,15 @@
+# Write your solution here
+def line(a, b):
+    print(a*b[0])
+    if 
+  
+
+
+
+
+
+
+
+# You can test your function by calling it within the following block
+if __name__ == "__main__":
+    line(5, "xxx")
