6fa188b33d60a5c2acb56d20bbfd56b01513d737
Explorjus <ja.kubson022@gmail.com>
2025-01-11 01:29:24 +0100

    nowe

--- part06-02_fruit_market/src/fruit_market.py
+++ part06-02_fruit_market/src/fruit_market.py
@@ -1,1 +1,14 @@
-# write your solution here
+def read_fruits():
+    lista = {}
+    with open("fruits.csv") as lista_owocow:
+        for linia in lista_owocow:
+            linia = linia.strip()  
+            nazwa, cena = linia.split(';')  
+            lista[nazwa] = float(cena)  
+
+
+    return lista
+
+
+if __name__ == "__main__":
+    print(read_fruits())
